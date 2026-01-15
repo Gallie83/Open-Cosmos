c4a8f239b9bcdca8adf6d75d7145d126762137f0
Gallie83 <keving2913@hotmail.com>
2026-01-15 21:37:04 +0000

    Level 2 complete, endpoint returns snapshots as JSON with optional start and end parameters and validation checks

--- api.py
+++ api.py
@@ -12,30 +12,46 @@ def datetime_valid(dt_str):
     except ValueError:
         return False
 
+# GET/ Returns all valid snapshots, accepts start and end times as parameters in 
 @app.route('/snapshots')
 def get_snapshots():
 
-    # First validate start and end parameters if they exist
-    start_time = request.args.get('start')
-    end_time = request.args.get('end')
+    try:
+        # First validate start and end parameters, if they exist
+        valid_params = {'start', 'end'}
+        params = set(request.args.keys())
+        unknown_params = params - valid_params
 
-    if start_time and not datetime_valid(start_time):
-        return jsonify({'error:', 'Invalid ISO start format'}), 400
+        if unknown_params:
+            return jsonify({'error': "Invalid parameters, only 'start' or 'end' accepted"}), 400
+
+        start_time = request.args.get('start')
+        end_time = request.args.get('end')
+
+        if start_time and not datetime_valid(start_time):
+            return jsonify({'error': 'Invalid ISO start format'}), 400
+        
+        if end_time and not datetime_valid(end_time):
+            return jsonify({'error': 'Invalid ISO end format'}), 400
+        
+        # Set start and end times or assign extreme values
+        start = start_time or '0000-01-01T00:00:00'
+        end = end_time or'9999-12-31T23:59:59'
+
+        valid_snapshots = []
+
+        if start > end:
+            return jsonify({'error': 'Start time must be before End time'}), 400
+
+        for snap in snapshot_storage:
+            if start <= snap['time'] <= end:
+                valid_snapshots.append(snap)
+                
+        return jsonify(valid_snapshots)
     
-    if end_time and not datetime_valid(end_time):
-        return jsonify({'error:', 'Invalid ISO end format'}), 400
-    
-    # Set start and end times or assign extreme values
-    start = start_time or '0000-01-01T00:00:00'
-    end = end_time or'9999-12-31T23:59:59'
+    except Exception as e:
+        return jsonify({'error': f'Server error: {e}'}), 500
 
-    valid_snapshots = []
-
-    for snap in snapshot_storage:
-        if start <= snap['time'] <= end:
-            valid_snapshots.append(snap)
-            
-    return jsonify(valid_snapshots)
 
 def start_api():
     app.run(port=8080)
