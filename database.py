6f4553fd9177f7095a9de84502cb6348aec6da06
Gallie83 <keving2913@hotmail.com>
2026-01-17 18:46:27 +0000

    database.py now uses connection pool to allow for up to 10 requests at a time

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 import psycopg2
+from psycopg2 import pool
+import logging
 
 # Retrieve environment variables from .env file
 load_dotenv()
@@ -11,12 +13,48 @@ DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 
-try:
-    conn = psycopg2.connect(database=DB_NAME,
-                            user=DB_USER,
-                            password=DB_PASSWORD,
-                            host=DB_HOST,
-                            port=DB_PORT)
-    print("Database connected successfully")
-except:
-    print("Database not connected successfully")
+connection_pool = None
+
+# Connects to database and creates tables when application is started
+def init_db():
+
+    logging.info("Connecting to database...")
+    global connection_pool
+
+    try:
+        # Create connection pool
+        connection_pool = psycopg2.pool.SimpleConnectionPool(
+            minconn=1,
+            maxconn=10,
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT
+        )
+        
+        logging.info("Connection pool created successfully")
+
+        # Requests connection from the pool
+        connection = connection_pool.getconn()
+        cursor = connection.cursor()
+
+        # Create table for valid_snapshots
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS valid_snapshots(
+                id SERIAL PRIMARY KEY,
+                time TIMESTAMP NOT NULL,
+                value REAL NOT NULL,
+                tags TEXT[] NOT NULL
+            )
+        """)
+
+        connection.commit()
+        logging.info("valid_snapshots table created successfully")
+
+        # Close cursor and return connection
+        cursor.close()
+        connection_pool.putconn(connection)
+
+    except Exception as e:
+        logging.error(f"Error connecting to database: {e}")
